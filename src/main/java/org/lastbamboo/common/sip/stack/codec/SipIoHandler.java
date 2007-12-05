9fe1c73f5691b51317a727947ac3ad6073e88133
adamfisk <a@littleshoot.org>
2007-12-05 16:44:02 +0000

    Added idle session handling
    git-svn-id: http://svn.littleshoot.org/svn/littleshoot/trunk/common/sip/stack@1811 8d7aff80-242d-0410-bcee-ed6f15385b3d

--- src/main/java/org/lastbamboo/common/sip/stack/codec/SipIoHandler.java
+++ src/main/java/org/lastbamboo/common/sip/stack/codec/SipIoHandler.java
@@ -3,8 +3,9 @@ package org.lastbamboo.common.sip.stack.codec;
 import java.io.IOException;
 
 import org.apache.mina.common.IdleStatus;
-import org.apache.mina.common.IoHandler;
+import org.apache.mina.common.IoHandlerAdapter;
 import org.apache.mina.common.IoSession;
+import org.apache.mina.util.SessionUtil;
 import org.lastbamboo.common.sip.stack.message.SipMessage;
 import org.lastbamboo.common.sip.stack.message.SipMessageVisitor;
 import org.lastbamboo.common.sip.stack.message.SipMessageVisitorFactory;
@@ -14,7 +15,7 @@ import org.slf4j.LoggerFactory;
 /**
  * Protocol handler for SIP messages.
  */
-public class SipIoHandler implements IoHandler
+public class SipIoHandler extends IoHandlerAdapter
     {
 
     private final Logger LOG = 
@@ -65,21 +66,30 @@ public class SipIoHandler implements IoHandler
         {
         }
 
+    public void sessionOpened(final IoSession session) throws Exception
+        {
+        }
+    
     public void sessionClosed(final IoSession session) throws Exception
         {
         }
 
+    @Override
     public void sessionCreated(final IoSession session) throws Exception
         {
+        SessionUtil.initialize(session);
+        
+        // The idle time is in seconds.  If there's been no traffic in either
+        // direction for awhile, we free the connection to limit load on the
+        // server.
+        session.setIdleTime(IdleStatus.BOTH_IDLE, 300);
         }
 
-    public void sessionIdle(final IoSession session, final IdleStatus idle) 
-        throws Exception
+    @Override
+    public void sessionIdle(final IoSession session, final IdleStatus status)
         {
+        LOG.warn("Killing idle session");
+        // Kill idle sessions.
+        session.close();
         }
-
-    public void sessionOpened(final IoSession session) throws Exception
-        {
-        }
-
     }
