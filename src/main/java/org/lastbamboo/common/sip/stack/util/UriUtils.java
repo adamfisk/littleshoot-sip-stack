6740cccf210375bbaf61f2c2c6ba4b7a828c1cef
adamfisk <a@littleshoot.org>
2010-05-18 06:05:19 +0000

    Tweaks to improve how we handle DNS SRV record lookups and changed to generalize SRV code
    git-svn-id: http://svn.littleshoot.org/svn/littleshoot/trunk/common/sip/stack@7008 8d7aff80-242d-0410-bcee-ed6f15385b3d

--- src/main/java/org/lastbamboo/common/sip/stack/util/UriUtils.java
+++ src/main/java/org/lastbamboo/common/sip/stack/util/UriUtils.java
@@ -10,126 +10,90 @@ public interface UriUtils
     /**
      * Returns a URI from a string.
      *
-     * @param uriString
-     *              The string containing a URI-formatting string.
+     * @param uriString The string containing a URI-formatting string.
      *
-     * @return
-     *              The URI represented by the passed string.
+     * @return The URI represented by the passed string.
      */
-    java.net.URI getUri
-            (String uriString);
+    java.net.URI getUri (String uriString);
 
     /**
      * Returns a URI from a host.
      *
-     * @param host
-     *              The host.
+     * @param host The host.
      *
-     * @return
-     *              The URI representing the given host.
+     * @return The URI representing the given host.
      */
-    java.net.URI getUriForHost
-            (String host);
+    java.net.URI getUriForHost (String host);
 
     /**
      * Returns a SIP URI for a given host of the form "sip:host".
      *
-     * @param host
-     *              The host.
+     * @param host The host.
      *
-     * @return
-     *              A URI for the given scheme, host, and port.
+     * @return A URI for the given scheme, host, and port.
      */
-    java.net.URI getSipUri
-            (String host);
+    java.net.URI getSipUri (String host);
 
     /**
      * Returns a SIP URI for a given host and port of the form "sip:host:port".
      *
-     * @param host
-     *              The host.
-     * @param port
-     *              The port.
+     * @param host The host.
+     * @param port The port.
      *
-     * @return
-     *              A URI for the given scheme, host, and port.
+     * @return A URI for the given scheme, host, and port.
      */
-    java.net.URI getSipUri
-            (String host,
-             int port);
+    java.net.URI getSipUri (String host, int port);
 
     /**
      * Returns a SIP URI for a given host and port of the form "sip:host:port".
      *
-     * @param host
-     *      The host.
-     * @param port
-     *      The port.
-     * @param transport
-     *      The transport to be used to connect.
-     *
-     * @return
-     *      A URI for the given scheme, host, and port.
+     * @param host The host.
+     * @param port The port.
+     * @param transport The transport to be used to connect.
+     * @return A URI for the given scheme, host, and port.
      */
-    java.net.URI getSipUri
-            (String host,
-             int port,
-             String transport);
+    java.net.URI getSipUri (String host, int port, String transport);
 
     /**
      * Returns a URI with a modified port.
      *
-     * @param uri
-     *              The original URI.
-     * @param port
-     *              The new port.
+     * @param uri The original URI.
+     * @param port The new port.
      *
-     * @return
-     *              The URI with the new port.
+     * @return The URI with the new port.
      */
-    java.net.URI getUriWithPort
-            (java.net.URI uri,
-             int port);
+    java.net.URI getUriWithPort (java.net.URI uri, int port);
 
     /**
      * Returns the user portion of a SIP URI (in the form of an opaque
      * java.net.URI).
      *
-     * @param sipUri
-     *      The URI.
+     * @param sipUri The URI.
      *
-     * @return
-     *      The user portion of the SIP URI.
+     * @return The user portion of the SIP URI.
      */
-    String getUserInSipUri
-            (java.net.URI sipUri);
+    String getUserInSipUri (java.net.URI sipUri);
 
     /**
      * Returns the host portion of a SIP URI (in the form of an opaque
      * java.net.URI).
      *
-     * @param sipUri
-     *      The URI.
+     * @param sipUri The URI.
      *
-     * @return
-     *      The host portion of the SIP URI.
+     * @return The host portion of the SIP URI.
      */
-    String getHostInSipUri
-            (java.net.URI sipUri);
+    String getHostInSipUri (java.net.URI sipUri);
     
     /**
      * Returns the user portion of a SIP URI (in the form of an opaque
      * java.net.URI) as a Little Shoot person identifier.
      *
-     * @param sipUri
-     *      The URI.
+     * @param sipUri The URI.
      *
-     * @return
-     *      The user portion of the SIP URI as a Little Shoot person
-     *      identifier..
+     * @return The user portion of the SIP URI as a Little Shoot person
+     * identifier..
      */
-    int getPersonIdInSipUri
-            (java.net.URI sipUri);
+    int getPersonIdInSipUri (java.net.URI sipUri);
 
     /**
      * Extracts the port from a SIP URI.  If the URI does not specify a port,
@@ -140,6 +104,6 @@ public interface UriUtils
      * @return The port in the SIP URI, or the default port if it is not 
      * specified.
      */
-    int getPortInSipUri(final URI uri);
+    int getPortInSipUri(URI uri);
 
     }
